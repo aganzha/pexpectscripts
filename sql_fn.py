2d5714468516e41251a7934d63c29c44bd658c0c
Alexey Ganzha <aganzha@yandex.ru>
2016-07-15 14:52:52 +0200

    more sql

---
+++ sql_fn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import pexpect
+from secret import password
+import os
+
+os.environ['LINES'] = "45"
+os.environ['COLUMNS'] = "120"
+
+if __name__ == '__main__':
+    child = pexpect.spawn('sudo su postgres')
+    child.setwinsize(640,480)
+    child.expect('.*password[^:]+: $', timeout=1)
+    child.sendline(password)
+    child.expect('postgres.+$', timeout=1)
+    child.sendline('psql')
+    child.expect('postgres=# $', timeout=1)
+    child.sendline('\c fnsite')
+    child.interact()    
